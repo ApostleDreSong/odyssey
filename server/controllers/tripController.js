541e7bca7df57a3e14e0358b4825e3e91b663879
Damilare Ademeso <ademesodamilare@gmail.com>
2020-08-21 09:31:57 +0100

    same station fare updated

--- server/controllers/tripController.js
+++ server/controllers/tripController.js
@@ -3,36 +3,50 @@ const db = require("../models");
 const controller = {
   calculate: async (req, res, next) => {
     try {
-      let rocket = await db.rockets.findOne({
-        where: {
-          id: req.body.rocket,
-        },
-      });
-      let transport;
-      if (req.body.startStation !== req.body.endStation) {
-        transport = rocket.transOrbitFare;
-      } else {
-        transport = rocket.intraOrbitFare;
-      }
-      ///Get landing station if man-made
-      let endSationDetails = await db.stations.findOne({
-        where: {
-          id: req.body.endStation,
-        },
-      });
+          let rocket = await db.rockets.findOne({
+            where: {
+              id: req.body.rocket,
+            },
+          });
+          let transport;
+          //find start station orbit
+          let startStationDetails = await db.stations.findOne({
+            where: {
+              id: req.body.startStation,
+            },
+          });
 
-      let endStationRoyalty = 0.0;
-      if (endSationDetails.type === "Manmade") {
-        endStationRoyalty = rocket.royalty;
-      }
-      let totalFare = Number(transport) + Number(endStationRoyalty);
+          //find end station orbit
+          let endStationDetails = await db.stations.findOne({
+            where: {
+              id: req.body.endStation,
+            },
+          });
 
-      res.status(200).send({
-        transportFare: Number(transport),
-        royalty: Number(endStationRoyalty),
-        totalFare,
-      });
-    } catch (e) {
+          if (startStationDetails.orbit !== endStationDetails.orbit) {
+            transport = rocket.transOrbitFare;
+          } else {
+            transport = rocket.intraOrbitFare;
+          }
+          ///Get landing station if man-made
+          let endSationDetails = await db.stations.findOne({
+            where: {
+              id: req.body.endStation,
+            },
+          });
+
+          let endStationRoyalty = 0.0;
+          if (endSationDetails.type === "Manmade") {
+            endStationRoyalty = rocket.royalty;
+          }
+          let totalFare = Number(transport) + Number(endStationRoyalty);
+
+          res.status(200).send({
+            transportFare: Number(transport),
+            royalty: Number(endStationRoyalty),
+            totalFare,
+          });
+        } catch (e) {
       next({
         status: 500,
         message: e,
